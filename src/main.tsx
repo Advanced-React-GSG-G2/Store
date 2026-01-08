c3cab2c1883b6e3426a9215e2e8e82dbde243d84
MohammedAl-Nabahin <moha.nabahin@gmail.com>
2026-01-08 14:26:11 +0200

    Use products provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import "./index.css";
+import App from "./App.tsx";
+import { ProductsProvider } from "./modules/Products/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
-      <App />
+      <ProductsProvider value="My Products Context">
+        <App />
+      </ProductsProvider>
     </MantineProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
