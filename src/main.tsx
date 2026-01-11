a21cf5da60beaa2cd9761a93597e48b345cc1002
yara <yarathabet66@gmail.com>
2026-01-11 10:37:43 +0200

    add (views+hooks+repository )change(main+index)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { ProductsProvider } from "./modules/Products/index.tsx";
+import App from "./App";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { createProductsModule } from "./modules/Products";
+
+const { Provider: ProductsProvider } = createProductsModule();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <ProductsProvider value="My Products Context">
+    <MantineProvider>
+      <ProductsProvider>
         <App />
       </ProductsProvider>
+    </MantineProvider>
   </StrictMode>
 );
--- src/modules/Products/index.tsx
+++ src/modules/Products/index.tsx
@@ -1,9 +1,11 @@
 import { createContext, useContext, type PropsWithChildren } from "react";
+import type { ProductsRepository } from "./repository/ProductsRepository";
+import { restProducts } from "./repository/restProducts";
 
-const ProductsContext = createContext<string | null>(null);
+const ProductsContext = createContext<ProductsRepository | null>(null);
 
 type ProductsProviderProps = PropsWithChildren<{
-  value: string;
+  value: ProductsRepository;
 }>;
 
 export const ProductsProvider = ({
@@ -20,9 +22,18 @@ export const ProductsProvider = ({
 export const useProducts = () => {
   const context = useContext(ProductsContext);
 
-  if (context === null) {
+  if (!context) {
     throw new Error("useProducts must be used within a ProductsProvider");
   }
 
   return context;
+};
+
+export const createProductsModule = () => {
+  const value = restProducts();
+  return {
+    Provider: ({ children }: PropsWithChildren) => (
+      <ProductsProvider value={value}>{children}</ProductsProvider>
+    ),
+  };
 };
