3cd1b19f5698bf26e95b2bc6a3f5e159ea6ebc09
MohammedAl-Nabahin <moha.nabahin@gmail.com>
2026-01-17 09:07:02 +0200

    Add missed imports

--- src/modules/Products/hooks/useGetAllProducts.ts
+++ src/modules/Products/hooks/useGetAllProducts.ts
@@ -1,29 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { useProducts } from "..";
+import { useMemo } from "react";
+import type { Product, ProductFilters } from "../entities/Product";
+import { applyProductFilters } from "../utils/applyProductFilters";
 
 const Get_ALL_PRODUCTS_QUERY_KEY = "products";
 
-export const useGetAllProducts = () => {
+export const useGetAllProducts = (filters?: ProductFilters) => {
   const { getAll } = useProducts();
   const { data, error, isLoading } = useQuery<Product[]>({
-    queryKey: ["products"],
+    queryKey: [Get_ALL_PRODUCTS_QUERY_KEY],
     queryFn: getAll,
     staleTime: 1000 * 60,
   });
-  
-  const allProducts = data ?? [];
-  const products = useMemo(
+
+  const allProducts = useMemo(() => data ?? [], [data]);
+  const filteredProducts = useMemo(
     () => applyProductFilters(allProducts, filters),
-    [allProducts, filters],
+    [allProducts, filters]
   );
 
+  const isEmpty = filteredProducts.length === 0;
+
   return {
-    products,
+    filteredProducts,
     allProducts,
     error,
     isLoading,
+    isEmpty,
   };
-
 };
 
 useGetAllProducts.queryKey = Get_ALL_PRODUCTS_QUERY_KEY;
